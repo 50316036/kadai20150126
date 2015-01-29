02b8817c4b1aac8939fbb85e2854c5e9a79731f9
50316036 <rn19940322@yahoo.co.jp>
2015-01-29 17:32:36 +0900

    20150126 submit

---
+++ src/graph/Graph_main.java
@@ -0,0 +1,23 @@
+package graph;
+
+import graph.Graph_View;
+
+public class Graph_main {
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+Graph_View graph = new Graph_View();
+
+graph.setBounds(5,5,655,455);
+
+graph.setVisible(true);
+
+
+
+	}
+
+}
